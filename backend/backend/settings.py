12cd8b6f6c27a5218921d70bb3e29766ca56e42e
TranThiet02 <ngothiet53@gmail.com>
2026-06-05 20:26:30 +0700

    last updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,12 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '192.168.1.3',
 ]
 
 
@@ -227,7 +226,6 @@ REST_AUTH = {
 # cors headers
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://192.168.1.3:3000",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
